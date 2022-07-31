8b70ec9e0a5fed1abd511cd72bbca7e65f5d8163
w0_0ss <wooseong.dev@gmail.com>
2022-07-31 17:47:56 +0900

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>devquest-template</title>
+      </Head>
+
+      <div>
+        <h1>hello world</h1>
+      </div>
+    </>
+  )
+}
+
+export default Home
